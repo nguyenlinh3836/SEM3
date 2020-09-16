850d793d19705775960dfdfe639d59cba1fac79a
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-09-16 10:11:56 +0700

    lab10

---
+++ Music/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Music.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Music
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<MusicItem> MusicItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MusicItems = new ObservableCollection<MusicItem>();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (POP.IsSelected)
+            {
+                MusicManager.GetMusics("POP", MusicItems);
+                TitleTextBlock.Text = "POP";
+            }
+            else if (RAP.IsSelected)
+            {
+                MusicManager.GetMusics("RAP", MusicItems);
+                TitleTextBlock.Text = "RAP";
+            }
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            POP.IsSelected = true;
+        }
+    }
+}
---
+++ Music/Model/MusicItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music.Model
+{
+    public class MusicItem
+    {
+        public int Id { get; set; }
+        
+        public string Image { get; set; }
+
+        public string Category { get; set;}
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+    }
+    public class MusicManager
+    {
+        public static void GetMusics(string category, ObservableCollection<MusicItem> musicItems)
+        {
+            var allItems = getMusicItems();
+
+            var filteredMusicItems = allItems.Where(p => p.Category == category).ToList();
+
+            musicItems.Clear();
+
+            filteredMusicItems.ForEach(p => musicItems.Add(p));
+        }
+
+        private static List<MusicItem> getMusicItems()
+        {
+            var items = new List<MusicItem>();
+
+            items.Add(new MusicItem() { Id = 1, Category = "POP", Title = "HAVANA", Artist = "Camila Cabello", Image = "Assets/havana.jpg" });
+            items.Add(new MusicItem() { Id = 2, Category = "POP", Title = "VORACITY", Artist = "Myth & Roid", Image = "Assets/voracity.jpg" });
+            items.Add(new MusicItem() { Id = 3, Category = "POP", Title = "PARALAXIUM", Artist = "Myth & Roid", Image = "Assets/PARALOXIUM.jpg" });
+
+            items.Add(new MusicItem() { Id = 4, Category = "RAP", Title = "OK", Artist = "Binz", Image = "Assets/ok.jpg" });
+            items.Add(new MusicItem() { Id = 5, Category = "RAP", Title = "BigCityBoy", Artist = "Binz", Image = "Assets/bigcity.jpg" });
+            return items;
+        }
+    }
+
+ }
